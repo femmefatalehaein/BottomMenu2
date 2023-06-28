a591aaf4ab044ed8ad3b10928e49a8b15904a2b7
femmefatalehaein <rmfls4359@gmail.com>
2023-06-28 15:10:46 +0900

    mainpage nested Scroll 변경 확인 및 recycler View 틀 잡기

--- app/src/main/java/kr/ac/duksung/bottommenu2/MainActivity.java
+++ app/src/main/java/kr/ac/duksung/bottommenu2/MainActivity.java
@@ -4,17 +4,23 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ListView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
@@ -33,6 +39,11 @@ public class MainActivity extends AppCompatActivity {
     
     //카테고리 버튼
 
+    //NestedScrollVeiw
+    RecyclerView recyclerView;
+    Adapter adapter;
+
+
 
 
     @Override
@@ -40,17 +51,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
         //객체 생성
         menu_fragment = new main_menu_fragment();
         account_fragment = new main_account_fragment();
         favorite_fragment = new main_favorite_fragment();
         community_fragment = new main_community_fragment();
-        
-        bottomNavigationView = findViewById(R.id.main_bottomNavigationView);
-        container = findViewById(R.id.container);
-        button = findViewById(R.id.button);
-        //button.bringToFront();
 
+        //bottomNavagationView
+        bottomNavigationView = findViewById(R.id.main_bottomNavigationView);
+
+        //fragment가 들어갈 container
+        container = findViewById(R.id.container);
+
+        //바로 보이는 첫화면의 container를 menu fragment로 채우기
+        //getSupportFragmentManager().beginTransaction().replace(R.id.container,menu_fragment).commit();
+
+        //가운데 floating button입니다.
+        button = findViewById(R.id.button);
+        // 상단 버튼누르면 해결된다.
         initTransactionEvent();
 
 
@@ -59,30 +80,27 @@ public class MainActivity extends AppCompatActivity {
              public boolean onNavigationItemSelected(@NonNull MenuItem item)
              {
                  switch (item.getItemId()){
-                     case R.id.item_menu:
+                     case R.id.item_1:
                          getSupportFragmentManager().beginTransaction().replace(R.id.container,menu_fragment).commit();
                          return true;
                      case R.id. item_2:
                          getSupportFragmentManager().beginTransaction().replace(R.id.container, favorite_fragment).commit();
+                         return true;
                      case R.id. item_4:
                          getSupportFragmentManager().beginTransaction().replace(R.id.container, community_fragment).commit();
+                         return true;
                      case R.id. item_5:
                          getSupportFragmentManager().beginTransaction().replace(R.id.container, account_fragment).commit();
-
-
-
-
+                         return true;
+                     default:
+                         getSupportFragmentManager().beginTransaction().replace(R.id.container,menu_fragment).commit();
                  }
-
-
-
                  return false;
              }
-
          });
 
 
-//가운데 QR인식 버튼 누르면, 큐알 인식창으로 넘어간다.
+        //가운데 QR인식 버튼 누르면, 큐알 인식 activity으로(SecondActivity) 넘어간다.
          button.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View view) {
@@ -90,6 +108,20 @@ public class MainActivity extends AppCompatActivity {
                  startActivity(intent);
              }
          });
+
+         //리사이클러 뷰
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+
+        //함께 배열 생성
+        adapter = new Adapter();
+        for(int i=0; i<10; i++){
+            String str = i +"번째 아이템";
+            adapter.setArrayData(str);
+        }
+
+        recyclerView.setAdapter(adapter);
+
     }
 
     private void initTransactionEvent() {
--- app/src/main/java/kr/ac/duksung/bottommenu2/main_menu_fragment.java
+++ app/src/main/java/kr/ac/duksung/bottommenu2/main_menu_fragment.java
@@ -2,27 +2,20 @@ package kr.ac.duksung.bottommenu2;
 
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link main_menu_fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class main_menu_fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public main_menu_fragment() {
         // Required empty public constructor
@@ -40,8 +33,7 @@ public class main_menu_fragment extends Fragment {
     public static main_menu_fragment newInstance(String param1, String param2) {
         main_menu_fragment fragment = new main_menu_fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +41,174 @@ public class main_menu_fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_menu_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main_menu_fragment, container, false);
+
+
+        Button form_list_button1 = (Button) rootView.findViewById(R.id.form_list_1);
+        Button form_list_button2 = (Button) rootView.findViewById(R.id.form_list_2);
+        Button form_list_button3 = (Button) rootView.findViewById(R.id.form_list_3);
+        Button form_list_button4 = (Button) rootView.findViewById(R.id.form_list_4);
+
+
+
+
+        form_list_button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("button", ": 1 on Click 작동");
+                Toast tMsg = Toast.makeText(rootView.getContext(),"버튼1",Toast.LENGTH_SHORT);
+                tMsg.show();
+
+            }
+        });
+
+        form_list_button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Button color
+                form_list_button1.setBackgroundResource(R.color.transparent);
+                form_list_button2.setBackgroundResource(R.drawable.category_selected);
+                form_list_button3.setBackgroundResource(R.color.transparent);
+                form_list_button4.setBackgroundResource(R.color.transparent);
+
+                // text color
+                form_list_button1.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.black));
+                form_list_button2.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.white));
+                form_list_button3.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.black));
+                form_list_button4.setTextColor(ContextCompat.getColor(rootView.getContext(), R.color.black));
+
+                Toast tMsg = Toast.makeText(rootView.getContext(),"버튼2",Toast.LENGTH_SHORT);
+            }
+        });
+
+        form_list_button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast tMsg = Toast.makeText(rootView.getContext(),"버튼3",Toast.LENGTH_SHORT);
+            }
+        });
+
+        form_list_button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast tMsg = Toast.makeText(rootView.getContext(),"버튼4",Toast.LENGTH_SHORT);
+            }
+        });
+
+        return rootView;
+
     }
+
+    private void setBtnColor(){
+
+    }
+/*
+    private void initTransactionEvent() {
+
+        final FirstFragment firstFragment = new FirstFragment();
+        final SecondFragment secondFragment = new SecondFragment();
+        final ThirdFragment thirdFragment = new ThirdFragment();
+        final FourthFragment fourthFragment = new FourthFragment();
+
+        Button form_list_button1 = (Button) findViewById(R.id.form_list_1);
+        Button form_list_button2 = (Button) findViewById(R.id.form_list_2);
+        Button form_list_button3 = (Button) findViewById(R.id.form_list_3);
+        Button form_list_button4 = (Button) findViewById(R.id.form_list_4);
+
+
+        getSupportFragmentManager().beginTransaction().add(R.id.form_list_fv, firstFragment).commit();
+
+
+        form_list_button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Button color
+                form_list_button1.setBackgroundResource(R.drawable.category_selected);
+                form_list_button2.setBackgroundResource(R.color.transparent);
+                form_list_button3.setBackgroundResource(R.color.transparent);
+                form_list_button4.setBackgroundResource(R.color.transparent);
+
+                // text color
+                form_list_button1.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+                form_list_button2.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+                form_list_button3.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+                form_list_button4.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.form_list_fv, firstFragment);
+                transaction.commit();
+            }
+        });
+
+        form_list_button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Button color
+                form_list_button1.setBackgroundResource(R.color.transparent);
+                form_list_button2.setBackgroundResource(R.drawable.category_selected);
+                form_list_button3.setBackgroundResource(R.color.transparent);
+                form_list_button4.setBackgroundResource(R.color.transparent);
+
+                // text color
+                form_list_button1.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+                form_list_button2.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+                form_list_button3.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+                form_list_button4.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.form_list_fv, secondFragment);
+                transaction.commit();
+            }
+        });
+
+        form_list_button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Button color
+                form_list_button1.setBackgroundResource(R.color.transparent);
+                form_list_button2.setBackgroundResource(R.color.transparent);
+                form_list_button3.setBackgroundResource(R.drawable.category_selected);
+                form_list_button4.setBackgroundResource(R.color.transparent);
+
+                // text color
+                form_list_button1.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+                form_list_button2.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+                form_list_button3.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+                form_list_button4.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.form_list_fv, thirdFragment);
+                transaction.commit();
+            }
+        });
+
+        form_list_button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Button color
+                form_list_button1.setBackgroundResource(R.color.transparent);
+                form_list_button2.setBackgroundResource(R.color.transparent);
+                form_list_button3.setBackgroundResource(R.color.transparent);
+                form_list_button4.setBackgroundResource(R.drawable.category_selected);
+
+                // text color
+                form_list_button1.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+                form_list_button2.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+                form_list_button3.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.black));
+                form_list_button4.setTextColor(ContextCompat.getColor(getApplicationContext(), R.color.white));
+
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.form_list_fv, fourthFragment);
+                transaction.commit();
+            }
+        });
+    }*/
+
+
 }
