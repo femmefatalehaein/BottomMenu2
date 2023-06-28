dc2d2577bd34f7bb2553c1725a1dbe1c42174b26
yeji1008 <willy019@naver.com>
2023-06-28 16:54:23 +0900

    add menu fragment + drawable

---
+++ app/src/main/java/kr/ac/duksung/bottommenu2/MenuItem.java
@@ -0,0 +1,60 @@
+package kr.ac.duksung.bottommenu2;
+
+import java.util.ArrayList;
+
+public class MenuItem {
+
+    int resourceId;
+    String name;
+    String price;
+    String benefit;
+
+    public MenuItem(int resourceId, String name, String price, String benefit) {
+        this.resourceId = resourceId;
+        this.name = name;
+        this.price= price;
+        this.benefit = benefit;
+    }
+
+    // 입력받은 숫자의 리스트생성
+    public static ArrayList<MenuItem> createContactsList(int numContacts) {
+        ArrayList<MenuItem> contacts = new ArrayList<MenuItem>();
+
+        for (int i = 1; i <= numContacts; i++) {
+            contacts.add(new MenuItem(R.drawable.test_img,"구테로이테", "3,000~ ", "포장방문시 1,000원 할인"));
+        }
+
+        return contacts;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public String getBenefit() {
+        return benefit;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    public void setBenefit(String benefit) {
+        this.benefit = benefit;
+    }
+}
