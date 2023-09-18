f76b157a8308324e8e51b6b9795cf56dc59ded8f
yeji1008 <willy019@naver.com>
2023-09-18 22:04:37 +0900

    account fragment layout + reward에 따른 image 변경

--- app/src/main/java/kr/ac/duksung/bottommenu2/main_account_fragment.java
+++ app/src/main/java/kr/ac/duksung/bottommenu2/main_account_fragment.java
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -72,7 +73,28 @@ public class main_account_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_account_fragment, container, false);
-       // qrtext = (TextView) findViewbyId
+        View view = inflater.inflate(R.layout.fragment_main_account_fragment, container, false);
+        ImageView treeImageView = view.findViewById(R.id.account_tree_iv);
+
+        // 가상의 리워드 값
+        int rewardValue = 250; // 임의로 설정
+
+        // 리워드 값에 따라 다른 이미지 설정
+        if (rewardValue >= 0 && rewardValue < 100) {
+            treeImageView.setImageResource(R.drawable.ic_seed);
+        } else if (rewardValue >= 100 && rewardValue < 200) {
+            treeImageView.setImageResource(R.drawable.ic_sprout);
+        } else if (rewardValue >= 200 && rewardValue < 300) {
+            treeImageView.setImageResource(R.drawable.ic_small_tree);
+        } else if (rewardValue >= 300 && rewardValue < 400) {
+            treeImageView.setImageResource(R.drawable.ic_tree);
+        } else if (rewardValue >= 400 && rewardValue < 500) {
+            treeImageView.setImageResource(R.drawable.ic_apple_tree);
+        } else {
+            // 기본 이미지 설정
+            treeImageView.setImageResource(R.drawable.ic_seed);
+        }
+
+        return view;
     }
 }
